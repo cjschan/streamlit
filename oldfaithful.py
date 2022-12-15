90123941d1e91650ae0fb00ed53dae5fbff8e99a
cjschan <cjschan@gmail.com>
2022-12-15 11:40:06 -0600

    Update oldfaithful.py

--- oldfaithful.py
+++ oldfaithful.py
@@ -19,13 +19,8 @@ hide = """
         """
 st.markdown(hide, unsafe_allow_html=True)
 
-WBCD = pd.read_csv("WisconsinBreastCancerDatabase.csv")
-WBCD.loc[WBCD['Diagnosis']=='B','Diagnosis']=0
-WBCD.loc[WBCD['Diagnosis']=='M','Diagnosis']=1
+geyser = pd.read_csv("oldfaithful.csv")
 
-# Store relevant columns as variables
-X = WBCD[['Radius mean']].values.reshape(-1, 1)
-y = WBCD[['Diagnosis']].values.reshape(-1, 1).astype(int)
 
 #Logistic regression predicting diagnosis from tumor radius
 logisticModel = LogisticRegression()
@@ -34,7 +29,11 @@ logisticModel.fit(X,np.ravel(y.astype(int)))
 col1, col2 = st.columns([1,3])
 
 with col1:
-    cutoff = st.slider('Probability cutoff',0.2, 0.8, 0.5,0.01)
+    clust_num = st.slider('Clusters', 1, 272, 1)
+    kmModel = KMeans(n_clusters = clust_num)
+    kmModel = kmModel.fit(geyser)
+    centroids = kmModel.cluster_centers_
+    clusters = kmModel.fit_predict(geyser[['Eruption', 'Waiting']])
     # yPredictedProb = logisticModel.predict_proba(X)[:,1]
     # yPredLowCutoff = []
     # for i in range(0,yPredictedProb.size):
@@ -47,27 +46,7 @@ with col1:
     # st.write("Recall: " + str(round(metrics.recall_score(y,yPredLowCutoff),2)))
 
 with col2:
-    #Graph logistic regression probabilities
-    fig, ax = plt.subplots()
-    x = [X.min(),X.max()]
-    y_val = [cutoff, cutoff]
-    x_val = (np.log(cutoff/(1-cutoff))+15.120902)/1.02475609
-    plt.scatter(X,y)
-    plt.plot(x, y_val, color='gray', linewidth=3)
-    plt.plot([x_val,x_val],[0,1], color='gray', linewidth=3)
-    # plt.text(X.min()+2,cutoff+0.1,"FN", fontsize="large")
-    # plt.text(X.min()+2,cutoff-0.1,"TN", fontsize="large")
-    # plt.text(X.max()-2,cutoff+0.1,"TP", fontsize="large")
-    # plt.text(X.max()-2,cutoff-0.1,"FP", fontsize="large")
-    xDelta = np.linspace(X.min(),X.max(),10000)
-    yPredicted = logisticModel.predict(X).reshape(-1,1).astype(int)
-    yDeltaProb = logisticModel.predict_proba(xDelta.reshape(-1,1))[:,1]
-    plt.plot(xDelta,yDeltaProb, color='red')
-    ax.set_xlabel('Radius mean',fontsize=14);
-    ax.set_ylabel('Probability of malignant tumor',fontsize=14);
+    p = sns.scatterplot(data=geyser, x='Eruption', y='Waiting', hue=clusters, style=clusters)
+    p.set_xlabel('Eruption time (min)', fontsize=14)
+    p.set_ylabel('Waiting time (min)', fontsize=14)
     st.pyplot(fig)
-    desc1 = "Description: A classification model using logistic regression  with a probability cutoff of "
-    desc2 = str(cutoff) + " will classify tumors with a radius mean of less than " + str(round(x_val,2))
-    desc3 = " as benign. Tumors with a radius mean of"
-    desc4 = " greater than or equal to " + str(round(x_val,2)) + " will be classified as malignant."
-    st.write(desc1 + desc2 + desc3 + desc4)
