dee535dc758c161d89acd7284ea25a7a72d81c8d
cjschan <cjschan@gmail.com>
2022-12-15 13:51:48 -0600

    Update oldfaithful.py

--- oldfaithful.py
+++ oldfaithful.py
@@ -43,7 +43,7 @@ with col2:
     plt.scatter(x=x_cent, y=y_cent, c="black", marker="X", s=120)
     st.pyplot(fig)
     desc1 = "Description: A scatter plot of the Old Faithful eruption data"
-    if clust_num==1
+    if clust_num==1:
         desc2 = "with " + str(clust_num) + " cluster. The within-cluster sum of "
     else:
         desc2 = "with " + str(clust_num) + " clusters. The within-cluster sum of "
