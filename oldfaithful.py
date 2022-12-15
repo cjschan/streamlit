dc95291b22bf75b33f93e965c48d49f3ababb8eb
cjschan <cjschan@gmail.com>
2022-12-15 11:51:15 -0600

    Update oldfaithful.py

--- oldfaithful.py
+++ oldfaithful.py
@@ -1,11 +1,8 @@
 import streamlit as st
 import pandas as pd
 import seaborn as sns
-import numpy as np
 import matplotlib.pyplot as plt
-from sklearn import metrics, svm
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
+from sklearn.cluster import KMeans
 
 hide = """
         <style>
