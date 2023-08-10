c33cb42229c5b6baaa6baf3592e0964a3e24d362
cjschan <cjschan@gmail.com>
2023-08-10 11:17:05 -0500

    Update spreadsheets.py

--- spreadsheets.py
+++ spreadsheets.py
@@ -9,17 +9,20 @@ from mitosheet.streamlit.v1 import spreadsheet
 st.set_page_config(layout="wide")
 st.title("Data Cleaning Verification")
 
-st.markdown("""
-This app only allows you to download data after it passes a series of data quality checks. After importing data, the app will run a series of checks against your data and prompt you with a set of data cleaning steps.
+col1, col2 = st.columns(2)
 
-To use the app, follow the mitosheet below:
-1. Click **Import** > **Import Files** and select an XLSX file from the `data` folder.
-2. Click the **Import Button**, and configure the import to skip rows depending on the file you choose.
-3. Use the Mitosheet to clean the data according to the prompts.
-4. Once all of the checks pass, download the csv file.
+with col1:
+    st.markdown("""
+    This app only allows you to download data after it passes a series of data quality checks. After importing data, the app will run a series of checks against your data and prompt you with a set of data cleaning steps.
 
-This app is meant to demo the mitosheet library. Learn more [here](https://trymito.io).
-""")
+    To use the app, follow the mitosheet below:
+    1. Click **Import** > **Import Files** and select an XLSX file from the `data` folder.
+    2. Click the **Import Button**, and configure the import to skip rows depending on the file you choose.
+    3. Use the Mitosheet to clean the data according to the prompts.
+    4. Once all of the checks pass, download the csv file.
+
+    This app is meant to demo the mitosheet library. Learn more [here](https://trymito.io).
+    """)
 
 
 CHECKS_AND_ERRORS = [
@@ -56,44 +59,45 @@ CHECKS_AND_ERRORS = [
     ),
 ]
 
-def run_data_checks_and_display_prompts(df):
-    """
-    Runs the data checks and displays prompts for the user to fix the data.
-    """
-    for check, error_message, help in CHECKS_AND_ERRORS:
-        if check(df):
-            st.error(error_message + " " + help)
-            return False
-    return True
+with col2:
+    def run_data_checks_and_display_prompts(df):
+        """
+        Runs the data checks and displays prompts for the user to fix the data.
+        """
+        for check, error_message, help in CHECKS_AND_ERRORS:
+            if check(df):
+                st.error(error_message + " " + help)
+                return False
+        return True
 
-@st.cache_data
-def convert_df(df):
-    return df.to_csv(index=False).encode('utf-8')
+    @st.cache_data
+    def convert_df(df):
+        return df.to_csv(index=False).encode('utf-8')
 
-# Display the data inside of the spreadsheet so the user can easily fix data quality issues.
-dfs, _ = spreadsheet(import_folder='./data')
+    # Display the data inside of the spreadsheet so the user can easily fix data quality issues.
+    dfs, _ = spreadsheet(import_folder='./data')
 
-# If the user has not yet imported data, prompt them to do so.
-if len(dfs) == 0:
-    st.info("Please import a file to begin. Click **Import** > **Import Files** and select a file from the `data` folder.")
+    # If the user has not yet imported data, prompt them to do so.
+    if len(dfs) == 0:
+        st.info("Please import a file to begin. Click **Import** > **Import Files** and select a file from the `data` folder.")
 
-    # Don't run the rest of the app if the user hasn't imported data.
-    st.stop()
+        # Don't run the rest of the app if the user hasn't imported data.
+        st.stop()
 
-# Run the checks on the data and display prompts
-df = list(dfs.values())[0]
-checks_passed = run_data_checks_and_display_prompts(df)
+    # Run the checks on the data and display prompts
+    df = list(dfs.values())[0]
+    checks_passed = run_data_checks_and_display_prompts(df)
 
-# If the data passes all checks, allow the user to download the data
-if checks_passed:
-    st.success("All checks passed! This data is clean, and ready to be downloaded.")
+    # If the data passes all checks, allow the user to download the data
+    if checks_passed:
+        st.success("All checks passed! This data is clean, and ready to be downloaded.")
 
-    csv = convert_df(df)
+        csv = convert_df(df)
 
-    st.download_button(
-        "Press to Download",
-        csv,
-        "mito_verified_data.csv",
-        "text/csv",
-        key='download-csv'
-    )
+        st.download_button(
+            "Press to Download",
+            csv,
+            "mito_verified_data.csv",
+            "text/csv",
+            key='download-csv'
+        )
