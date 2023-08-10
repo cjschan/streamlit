4fb85aa5b7a0b3fa9e5f89fdf74e25320c0ee9ce
cjschan <cjschan@gmail.com>
2023-08-10 11:18:28 -0500

    Update spreadsheets.py

--- spreadsheets.py
+++ spreadsheets.py
@@ -9,7 +9,7 @@ from mitosheet.streamlit.v1 import spreadsheet
 st.set_page_config(layout="wide")
 
 
-col1, col2 = st.columns(2)
+col1, col2 = st.columns([2,3])
 
 with col1:
     st.title("Data Cleaning Verification")
