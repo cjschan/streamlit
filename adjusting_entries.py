018ed26ce54ff6609ed161016bb4113b7d2e2bea
cjschan <cjschan@gmail.com>
2023-08-09 22:22:53 -0500

    Update adjusting_entries.py

--- adjusting_entries.py
+++ adjusting_entries.py
@@ -21,27 +21,4 @@ hide = """
 st.markdown(hide, unsafe_allow_html=True)
 
 question = "BlueButton Marketing, Inc. worked in the office for the month of September. The amount of electricity the company used equaled $12,000 for the month. On October 1, the business received the bill and wrote a check to the electric company on October 2."
-
-
-
-if "visibility" not in st.session_state:
-    st.session_state.visibility = "visible"
-    st.session_state.disabled = False
-
-col1, col2 = st.columns(2)
-
-with col1:
-    st.checkbox("Disable selectbox widget", key="disabled")
-    st.radio(
-        "Set selectbox label visibility 👉",
-        key="visibility",
-        options=["visible", "hidden", "collapsed"],
-    )
-
-with col2:
-    option = st.selectbox(
-        "How would you like to be contacted?",
-        ("Email", "Home phone", "Mobile phone"),
-        label_visibility=st.session_state.visibility,
-        disabled=st.session_state.disabled,
-    )
+st.write(question)
